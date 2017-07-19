4235938f487a73deb061f249f31312849a513aba
natalya118 <nutblog88@gmail.com>
2017-07-19 14:10:16 +0300

    added db context

---
+++ UklonBot/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace UklonBot.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+            : base("UklonBotConn")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
